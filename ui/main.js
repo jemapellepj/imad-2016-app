f013a6a29657f20ad131d5b21aaa62a369e559f2
jemapellepj <381994pj@gmail.com>
2016-10-11 18:31:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
  var img=document.getElementById('madi');
  var marginLeft=0;
- function moveRight(){
+ function moveRight () {
      marginLeft=marginLeft +10;
-     img.style.maginLeft=marginLeft +'px';
+     img.style.marginLeft=marginLeft +'px';
  }
  img.onclick=function(){
   var interval=setInterval(moveRight,100)   ;
